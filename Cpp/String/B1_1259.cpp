18c2de65cc797231e1cf8ff4e2c388d261daad33
ggomabbang <badawa7@naver.com>
2023-07-20 21:12:48 +0900

    Bronze_1_PalindromeNumber Clear

--- Cpp/String/B1_1259.cpp
+++ Cpp/String/B1_1259.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(void){
 
-    int n;
+    string word;
+    int i, index;
+    int palindrom;
+    cin>>word;
 
-    cin>>n;
+    while(word != "0"){
+        i = 0;
+        index = word.length()-1;
+        palindrom = 1;
+        while((i != index)){
+            if (word[i] != word[index]){
+                palindrom = 0;
+                break;
+            }
+            if (i+1 == index) break;
+            i++;
+            index--;
+        }
 
-    cout<<"hello world!"<<endl;
+        if (palindrom){
+            cout<<"yes\n";
+        }
+        else {
+            cout<<"no\n";
+        }
+
+        cin>>word;
+    }
+
     return 0;
 }
