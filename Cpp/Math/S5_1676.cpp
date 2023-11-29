919173e9c4cd61f7e52f92103a177016feeeecd9
ggomabbang <badawa7@naver.com>
2023-11-29 19:54:34 +0900

    S5 - 팩토리얼 0의 개수 cpp

---
+++ Cpp/Math/S5_1676.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    int n;
+    int n_fact=1;
+    int count=0;
+
+    cin>>n;
+    
+    for (int i=1; i<=n; i++){
+        n_fact *= i;
+        while (n_fact % 10 == 0){
+            count++;
+            n_fact /= 10;
+        }
+        if (n_fact > 1000000){
+            n_fact %= 1000000;
+        }
+    }
+
+    cout<<count;
+
+    return 0;
+}
