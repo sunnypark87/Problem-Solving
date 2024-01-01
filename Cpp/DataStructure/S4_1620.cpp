b72d5be61f32b2f46bf04e146cae25ecbd4a518a
ggomabbang <badawa7@naver.com>
2024-01-01 11:59:34 +0900

    S4. 1620 - 나는야 포켓몬 마스터 이다솜 cpp done

---
+++ Cpp/DataStructure/S4_1620.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main(void) {
+
+    int n, m;
+    string name, input;
+    int no;
+    unordered_map<string, int> names;
+    unordered_map<int, string> numbers;
+
+    cin>>n>>m;
+    for (int i=1; i<=n; i++) {
+        cin>>name;
+        names[name] = i;
+        numbers[i] = name; 
+    }
+
+    for (int i=0; i<m; i++) {
+        cin>>input;
+        try {
+            no = stoi(input);
+            cout<<numbers[no]<<'\n';
+        }
+        catch (const invalid_argument& e) {
+            cout<<names[input]<<'\n';
+        }
+    }
+
+    return 0;
+}
