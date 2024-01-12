8ed768c49d13cf53b8909697581a3f9aa833507e
ggomabbang <badawa7@naver.com>
2024-01-12 21:25:10 +0900

    S2. 11279 - 최대 힙 cpp done

---
+++ Cpp/DataStructure/S2_11279.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+
+    int n, input;
+    priority_queue<int> max_heap;
+
+    cin>>n;
+    for (int i=0; i<n; i++) {
+        cin>>input;
+        if (input == 0) {
+            if (max_heap.empty()) {
+                cout<<"0\n";
+            }
+            else {
+                cout<<max_heap.top()<<"\n";
+                max_heap.pop();
+            }
+        }
+        else {
+            max_heap.push(input);
+        }
+    }
+
+
+    return 0;
+}
