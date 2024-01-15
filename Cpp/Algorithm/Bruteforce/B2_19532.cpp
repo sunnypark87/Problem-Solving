e317524d62d04cba029f66707805c72115d3db98
ggomabbang <badawa7@naver.com>
2024-01-15 10:17:47 +0900

    B2. 19532 - 수학은 비대면 강의입니다 cpp done

---
+++ Cpp/Algorithm/Bruteforce/B2_19532.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    int a, b, c;
+    int d, e, f;
+    int x, y;
+
+    cin>>a>>b>>c;
+    cin>>d>>e>>f;
+
+    for (int i=-999; i<=999; i++) {
+        for (int j=-999; j<=999; j++) {
+            if (a*i + b*j == c && d*i + e*j == f) {
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    cout<<x<<" "<<y;
+
+    return 0;
+}
