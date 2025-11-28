5b2900241958c48372a0034a91b51455406a276e
Jaeseon Park <73125851+sunnypark87@users.noreply.github.com>
2025-11-28 21:13:35 +0900

    S3 15652 N과 M4 최적화
    배열을 전역변수로 해도 충돌이 일어나지 않음
    ios::sync_with_stdio의 중요성

--- Baekjoon/Cpp/02_Algorithm/01_Bruteforce_Backtracking/S3_15652_N과M4.cpp
+++ Baekjoon/Cpp/02_Algorithm/01_Bruteforce_Backtracking/S3_15652_N과M4.cpp
@@ -3,30 +3,28 @@
 using namespace std;
 
 int n, m; 
-void combination(vector<int> sequence, int prev, int step) {
+vector<int> sequence(9, 0);
+void combination(int prev, int step) {
     if (step == m) {
-        for (auto num : sequence) {
-            cout << num << ' ';
+        for (int i=0; i<m; i++) {
+            cout << sequence[i] << ' ';
         }
         cout << '\n';
         return;
     }
 
-    for (int i=1; i<=n; i++) {
-        if (prev <= i) {
-            sequence.push_back(i);
-            combination(sequence, i, step+1);
-            sequence.pop_back();
-        }
+    for (int i=prev; i<=n; i++) {
+        sequence[step] = i;
+        combination(i, step+1);
     }
 }
 
 int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     cin >> n >> m;
-
-    vector<int> sequence;
-
-    combination(sequence, 0, 0);
+    combination(1, 0);
     
     return 0;
 }
